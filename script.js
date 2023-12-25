3ed639161e93cfcba7e4be04c80ad6d6eb6e02cb
Huemman <mehmehkyuto@gmail.com>
2023-12-25 05:38:15 -0800

    functional searchbar

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
 const link = "https://image.tmdb.org/t/p/w440_and_h660_face";
-const api_key = "1bfdbff05c2698dc917dd28c08d41096"
+const api_key = "1bfdbff05c2698dc917dd28c08d41096";
+const input = document.getElementById('search');
+const form = document.getElementById('top-bar-form');
+const container = document.getElementById("container");
 
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  console.log(input.value);
+  search(input.value)
+})
 const movies = async () => {
-  let movieData = [];
-  const data = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`);
-  movieData.push(await data.json());
-  console.log(movieData);
-  const movieCards = movieData.map(data => `
-    ${data.results.map((data)=>(`
+  const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`);
+  const {results} = await res.json();
+  const movieCards =  results.map(({backdrop_path, original_title})=>(`
     <div class="content">
-      <img src="${link}${data.backdrop_path}">
-      <p>${data.original_title}</p>
+      <img src="${link}${backdrop_path}">
+      <p>${original_title}</p>
     </div>
-    `)).join("")}
-  `);
-  document.getElementById("container").innerHTML = movieCards;
+    `)).join("")
+  container.innerHTML = movieCards;
 };
 
 movies()
+
+const search = async (title) => {
+  try{
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`);
+    const {results} = await res.json();
+    console.log(results);
+    const movieCards = results.map(({backdrop_path, original_title}) => (`
+    <div class="content">
+      <img src="${link}${backdrop_path}">
+      <p>${original_title}</p>
+    </div>
+    `)).join("");
+    
+
+    if(results.length > 0){
+      container.innerHTML = movieCards;
+      return
+    }
+    console.log("WALANG GANON");
+    alert("PAKYU")  
+    }
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
