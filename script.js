cab619720d313e2dd277f26c716b3fcf9e34e2ce
Huemman <mehmehkyuto@gmail.com>
2023-12-28 21:55:23 -0800

    added page increment and movie rating

--- script.js
+++ script.js
@@ -5,19 +5,34 @@ const form = document.getElementById('top-bar-form');
 const container = document.getElementById("container");
 const notFound = document.getElementById('not-found');
 const detailsContainer = document.getElementById('details');
+const nxtBtn = document.getElementById('next');
+const prevBtn = document.getElementById('prev');
+let page = 1;
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   search(input.value)
 })
 
-const movies = async () => {
-  const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=1`);
+function nxtPage() {
+  page++;
+  movies()
+}
+
+function prevPage() {
+  if(page > 1){
+    page--;
+  }
+  movies()
+}
+
+const movies = async () => {  
+  const res = await fetch(`https://api.themoviedb.org/3/movie/upcoming?api_key=${api_key}&language=en-US&page=${page}`);
   const {results} = await res.json();
-  const movieCards =  results.map(({backdrop_path, original_title, id})=>(`
+  const movieCards =  results.map(({poster_path, title, id})=>(`
     <div ondblclick="movieDetails(${id})" class="content">
-        <img src="${link}${backdrop_path}">
-        <p>${original_title}</p>
+        <img src="${link}${poster_path}">
+        <p>${title}</p>
     </div>
     `)).join("")  
   container.innerHTML = movieCards; 
@@ -29,35 +44,42 @@ const movieDetails = async (id) => {
     const details = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=en-US`);
     const similar = await fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=${api_key}&language=en-US&page=1`);
     const {results} = await similar.json();
-    const {backdrop_path, overview} = await details.json();
+    const {backdrop_path, overview, vote_average, vote_count} = await details.json();
     const deck = `
-    <div class="movie-deck">
-      <img src="${link}${backdrop_path}"> 
-      <p>${overview}</p>
+    <div class="overview">
+      <img src="${link}${backdrop_path}" class="clicked-img"> 
+      <div class="text-ov">
+        <h2>About</h2>  
+        <p>${overview}</p>
+      </div>
     </div> 
+    <p>Rating: <strong>${vote_average}/10</strong>  ${vote_count}voted</p>
+    <h2>Similar Deck o'Movie Cards</h2>
     `;
-    const similars = results.map(({backdrop_path, original_title}) => (`
-    <div class="movie-deck">
-      <img src="${link}${backdrop_path}"> 
-      <p>${original_title}</p>
+    const similars = results.map(({poster_path, title, id}) => (`
+    <div ondblclick="movieDetails(${id})" class="content">
+      <img src="${link}${poster_path}">
+      <p>${title}</p> 
     </div> 
     `)).join("");
     container.innerHTML = similars;
     detailsContainer.innerHTML = deck;
+    nxtBtn.style.display = 'none';
+    prevBtn.style.display = 'none';
   } catch (error) {
     console.log('Change code');
   }
   
 };
 
-const search = async (title) => {
+const search = async (movieName) => {
   try{
-    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${title}`);
+    const res = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${movieName}`);
     const {results} = await res.json();
-    const movieCards = results.map(({backdrop_path, original_title}) => (`
-    <div class="content">
-      <img src="${link}${backdrop_path}">
-      <p>${original_title}</p>
+    const movieCards = results.map(({poster_path, title, id}) => (`
+    <div ondblclick="movieDetails(${id})" class="content">
+      <img src="${link}${poster_path}">
+      <p>${title}</p>
     </div>
     `)).join("");
     
@@ -71,6 +93,8 @@ const search = async (title) => {
     <p><span class="word-2">Deck</span> Not Found</p>
     </div>`;
       }
+    nxtBtn.style.display = 'none';
+    prevBtn.style.display = 'none';
     }
   catch (error) {
     console.log(error);
